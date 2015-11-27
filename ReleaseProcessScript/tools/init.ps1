3fb32016e1b4e91566a1b1fc49811ecb5b87fd17
Florian Decker <florian.decker@rubicon.eu>
2015-11-27 12:49:31 +0100

    add cmdlet help

--- ReleaseProcessScript/tools/init.ps1
+++ ReleaseProcessScript/tools/init.ps1
@@ -1,3 +1,5 @@
 param($installPath, $toolsPath, $package)
 
-Import-Module (Join-Path $toolsPath main_functions.psm1) -DisableNameChecking -Force
+Import-Module (Join-Path $toolsPath ReleaseProcessScript.psm1) -DisableNameChecking -Force
+
+Write-Host "Type 'get-help ReleaseProcessScript' to see all available ReleaseProcessScript commands"
